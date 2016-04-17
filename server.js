ed842c7e61364a16279942db3539d4d2e90dcebb
Benzion Blech <benzionblech@gmail.com>
2016-04-17 19:07:55 -0400

    Initialize repo

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAuthentication: function(req, res, next){
+		console.log('Private route hit!')
+		next();
+	},
+	logger: function(req, res, next) {
+		console.log('Request: ' + req.method + ' ' + new Date().toString());
+		next();
+	}
+}
+
+app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+app.get('/about', function(req, res){
+	res.send('about us!');
+})
+
+app.use(express.static(__dirname + "/public"))
+
+
+app.listen(PORT, function(){
+	console.log('server has started at PORT ' + PORT);
+});
